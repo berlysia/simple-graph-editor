297c2f6eec1a42e48d8180553ce152000b507eac
berlysia <berlysia@gmail.com>
2023-04-19 02:02:47 +0900

    it works(TM)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,220 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer, useRef, useState } from "react";
+import { GraphNodeProps, GraphNode, GraphNodeRef } from "./GraphNode";
+
+// 2点間の角度を求める
+const getAngle = (
+  from: { x: number; y: number },
+  to: { x: number; y: number }
+) => {
+  return Math.atan2(to.y - from.y, to.x - from.x);
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fieldRef = useRef<HTMLDivElement>(null);
+
+  // 強制rerender関数
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  // GraphNodeのIDを管理する
+  const [counter, countup] = useReducer((x) => x + 1, 0);
+
+  // GraphNodeを可変個管理する
+  const [nodes, setNodes] = useState<GraphNodeProps[]>([]);
+
+  // GraphNodeへの参照を保持する
+  const [nodeRefs] = useState<Map<number, GraphNodeRef>>(() => new Map());
+
+  const [labelValue, setLabelValue] = useState("");
+
+  // GraphNodeの間に線を引く
+  const [lines, setLines] = useState<{ from: number; to: number }[]>([]);
+  const [lineFrom, setLineFrom] = useState<number | null>(null);
+
+  // 右クリックしたときのコールバック
+  const onRightClick = (nodeId: number) => {
+    setLineFrom(nodeId);
+  };
+
+  // 右クリックしている間に左クリックしたときのコールバック
+  const onLeftClick = (nodeId: number) => {
+    if (lineFrom === null) {
+      return;
+    }
+    setLines([...lines, { from: lineFrom, to: nodeId }]);
+    setLineFrom(null);
+  };
 
   return (
-    <div className="App">
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            value={labelValue}
+            onChange={(e) => {
+              setLabelValue(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              setNodes([...nodes, { label: labelValue, nodeId: counter }]);
+              countup();
+              setLabelValue("");
+            }}
+          >
+            Add Node
+          </button>
+        </form>
+        <button
+          onClick={() => {
+            // 半径はノード数に応じて広がる
+            const radius = 100 + 10 * nodes.length;
+
+            // ウィンドウの中心を原点とする
+            const center = {
+              x: window.innerWidth / 2,
+              y: window.innerHeight / 2,
+            };
+
+            const angle = (2 * Math.PI) / nodes.length;
+            nodes.forEach((node, i) => {
+              const x = center.x + radius * Math.cos(angle * i);
+              const y = center.y + radius * Math.sin(angle * i);
+              nodeRefs.get(node.nodeId)?.setPosition({ x, y });
+            });
+            forceUpdate();
+          }}
+        >
+          Arrange Nodes
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/* ノードをすべて消す */}
+        <button
+          onClick={() => {
+            setNodes([]);
+            setLines([]);
+          }}
+        >
+          Clear
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <textarea
+          value={
+            /*
+              1行目はノードのラベルを表す
+              2行目と3行目はインデックスで対応関係にあり、2行目がfrom、3行目がtoを表す
+            */
+            [
+              nodes.map((node) => node.label),
+              // fromはnodeIdなのでインデックスに変換する
+              lines.map((line) =>
+                nodes.findIndex((x) => x.nodeId === line.from)
+              ),
+              // toはnodeIdなのでインデックスに変換する
+              lines.map((line) => nodes.findIndex((x) => x.nodeId === line.to)),
+            ].join("\n")
+          }
+          readOnly
+          style={{ height: "3em" }}
+        />
+      </div>
+      <div ref={fieldRef} style={{ position: "relative" }}>
+        {nodes.map((node) => (
+          <GraphNode
+            ref={(x) =>
+              x ? nodeRefs.set(node.nodeId, x) : nodeRefs.delete(node.nodeId)
+            }
+            onRightClick={onRightClick}
+            onLeftClick={onLeftClick}
+            onMove={
+              // ノードを移動したときに線を引き直す
+              (nodeId) => {
+                forceUpdate();
+              }
+            }
+            key={node.nodeId}
+            nodeId={node.nodeId}
+            label={node.label}
+            fieldRef={fieldRef}
+          />
+        ))}
+        {lines.map((line) => {
+          const fromRaw = nodeRefs.get(line.from)?.getPosition();
+          const toRaw = nodeRefs.get(line.to)?.getPosition();
+          if (!fromRaw || !toRaw) {
+            return null;
+          }
+
+          // fromRawからtoRawへの角度を求める
+          const angle = getAngle(fromRaw, toRaw);
+
+          // fromRawからtoRawへのベクトルを求める
+          const vector = {
+            x: toRaw.x - fromRaw.x,
+            y: toRaw.y - fromRaw.y,
+          };
+
+          // fromRawからtoRawへのベクトルの長さを求める
+          const length = Math.sqrt(vector.x ** 2 + vector.y ** 2);
+
+          // fromRawからtoRawへのベクトルの単位ベクトルを求める
+          const unitVector = {
+            x: vector.x / length,
+            y: vector.y / length,
+          };
+
+          // fromRawからtoRawへのベクトルの単位ベクトルに対して、
+          // GraphNodeの半径分だけずらした位置をfromとする
+          const from = {
+            x: fromRaw.x + unitVector.x * 50,
+            y: fromRaw.y + unitVector.y * 50,
+          };
+
+          // fromRawからtoRawへのベクトルの単位ベクトルに対して、
+          // GraphNodeの半径分だけずらした位置をtoとする
+          const to = {
+            x: toRaw.x - unitVector.x * 50,
+            y: toRaw.y - unitVector.y * 50,
+          };
+
+          return (
+            <>
+              <div
+                key={`${line.from}-${line.to}-body`}
+                style={{
+                  position: "absolute",
+                  border: "1px solid black",
+                  transformOrigin: "0 0",
+                  transform: `rotate(${Math.atan2(
+                    to.y - from.y,
+                    to.x - from.x
+                  )}rad)`,
+                  width: Math.sqrt((from.x - to.x) ** 2 + (from.y - to.y) ** 2),
+                  left: from.x,
+                  top: from.y,
+                }}
+              />
+              <div
+                key={`${line.from}-${line.to}-head`}
+                style={{
+                  position: "absolute",
+                  backgroundColor: "transparent",
+                  borderTop: "3px solid black",
+                  borderRight: "3px solid black",
+                  transformOrigin: "center",
+                  transform: `rotate(${angle + (1 / 4) * Math.PI}rad)`,
+                  width: 10,
+                  height: 10,
+                  left: to.x - 5,
+                  top: to.y - 5,
+                }}
+              />
+            </>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/GraphNode.tsx
@@ -0,0 +1,127 @@
+import React, {
+  forwardRef,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+
+export type GraphNodeRef = {
+  setPosition: (position: { x: number; y: number }) => void;
+  getPosition: () => { x: number; y: number };
+};
+
+export const GraphNode = forwardRef(function GraphNode(
+  {
+    label,
+    nodeId,
+    onRightClick,
+    onLeftClick,
+    onMove,
+    fieldRef,
+  }: {
+    label: string;
+    nodeId: number;
+    onRightClick: (nodeId: number) => void;
+    onLeftClick: (nodeId: number) => void;
+    onMove: (nodeId: number, position: { x: number; y: number }) => void;
+    fieldRef: React.RefObject<HTMLDivElement>;
+  },
+  forwardedRef: React.Ref<GraphNodeRef>
+) {
+  // ドラッグアンドドロップで親要素の中を移動するコンポーネント
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      const rect = ref.current.getBoundingClientRect();
+      // 初期位置は要素の中心座標
+      setPosition({ x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 });
+    }
+  }, []);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (isDragging) {
+      // 親要素の中での座標にするために、親要素の位置を引く
+      if (fieldRef.current) {
+        const fieldRect = fieldRef.current.getBoundingClientRect();
+        const position = {
+          x: e.clientX - fieldRect.x,
+          y: e.clientY - fieldRect.y,
+        };
+
+        setPosition(position);
+        onMove(nodeId, position);
+      }
+    }
+  };
+
+  useLayoutEffect(() => {
+    if (forwardedRef) {
+      if (typeof forwardedRef === "function") {
+        forwardedRef({ setPosition, getPosition: () => position });
+      } else {
+        // @ts-expect-error
+        forwardedRef.current = { setPosition };
+      }
+    }
+  }, [forwardedRef]);
+
+  useEffect(() => {
+    if (fieldRef.current) {
+      // @ts-expect-error
+      fieldRef.current.addEventListener("mousemove", handleMouseMove);
+    }
+    return () => {
+      if (fieldRef.current) {
+        // @ts-expect-error
+        fieldRef.current.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, [isDragging]);
+
+  return (
+    <div
+      ref={ref}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        onRightClick(nodeId);
+      }}
+      onClick={() => {
+        onLeftClick(nodeId);
+      }}
+      style={{
+        position: "absolute",
+        // positionの位置を要素の中心にするために、要素の大きさの半分を引く
+        left: position.x - 50,
+        top: position.y - 50,
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        backgroundColor: "red",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        userSelect: "none",
+      }}
+    >
+      {label}#{nodeId}
+    </div>
+  );
+});
+export type GraphNodeProps = {
+  label: string;
+  nodeId: number;
+};
